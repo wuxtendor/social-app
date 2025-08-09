9b850f1f0eef8efeda757e8c478c136d6391db2a
wuxtendor <wuxtendor@gmail.com>
2025-08-09 23:06:32 +0500

    fix bugs 1.0

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = 5000;
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // <-- Use the environment variable or default to localhost
     methods: ["GET", "POST"]
   }
 });
